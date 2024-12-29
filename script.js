239e1369a19df94c10f0e12d5c6b12929c448d52
guillermoSoftwareEngineer <guillermovasbendev@gmail.com>
2024-12-29 11:46:37 -0500

    send two menus responsive

---
+++ script.js
@@ -0,0 +1,66 @@
+
+/* inicio logica de menu 1 */
+
+document.querySelector(".menu-toggle-basic").addEventListener("click", () => {
+    document.querySelector(".nav-basic").classList.toggle("active");
+  });
+  
+
+/* inicio logica de menu 1 */
+
+/* inicio logica de menu 2 */
+
+document.querySelector(".menu-toggle-basic-2").addEventListener("click", () => {
+    document.querySelector(".nav-basic-2").classList.toggle("active");
+});
+
+
+/* inicio logica de menu 2 */
+
+/* inicio logica de menu 3 */
+
+
+
+/* inicio logica de menu 3 */
+
+/* inicio logica de menu 4 */
+
+
+
+/* inicio logica de menu 4 */
+
+/* inicio logica de menu 5 */
+
+
+
+/* inicio logica de menu 5 */
+
+/* inicio logica de menu 6 */
+
+
+
+/* inicio logica de menu 6 */
+
+/* inicio logica de menu 7 */
+
+
+
+/* inicio logica de menu 7 */
+
+/* inicio logica de menu 8 */
+
+
+
+/* inicio logica de menu 8 */
+
+/* inicio logica de menu 9 */
+
+
+
+/* inicio logica de menu 9 */
+
+/* inicio logica de menu 10 */
+
+
+
+/* inicio logica de menu 10 */
