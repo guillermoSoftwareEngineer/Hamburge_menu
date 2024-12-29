46cf128aab2b155aa46f91d429274e812ebcbfb8
guillermoSoftwareEngineer <guillermovasbendev@gmail.com>
2024-12-29 17:57:17 -0500

    send two more menus for the proyect

--- script.js
+++ script.js
@@ -19,15 +19,41 @@ document.querySelector(".menu-toggle-basic-2").addEventListener("click", () => {
 
 /* inicio logica de menu 3 */
 
+const toggle = document.querySelector(".menu-toggle-modern");
+const nav = document.querySelector(".nav-modern");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  toggle.classList.toggle("active");
+});
 
 
 /* inicio logica de menu 3 */
 
 /* inicio logica de menu 4 */
 
+// Selecciona el menú y el icono
+
+// Seleccionar elementos del menú 4
+const menuToggle4 = document.querySelector(".menu-container-4 .menu-toggle-modern-4");
+const nav4 = document.querySelector(".menu-container-4 .nav-modern-4");
+// Seleccionar el botón "X" dentro del menú desplegable
+const closeButton4 = document.querySelector(".menu-container-4 .close-menu-4");
+
+// Alternar la clase "active" al hacer clic
+menuToggle4.addEventListener("click", () => {
+    menuToggle4.classList.toggle("active");
+    nav4.classList.toggle("active");
+});
+
+// Agregar evento al botón "X" para cerrar el menú
+closeButton4.addEventListener("click", () => {
+    nav4.classList.remove("active");
+    menuToggle4.classList.remove("active");
+});
 
 
-/* inicio logica de menu 4 */
+/* final logica de menu 4 */
 
 /* inicio logica de menu 5 */
 
